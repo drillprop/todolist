468cbb6454e92a8d4a244d96f0944be81d4ceb8c
drillprop <drylbartosz@gmail.com>
2018-07-16 23:31:11 +0200

    added Login component

---
+++ src/Components/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Login extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" placeholder="Username" />
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" placeholder="Password" />
+        <Link to="/home">
+          <button type="submit"> Log In</button>
+        </Link>
+      </form>
+    );
+  }
+}
+
+export default Login;
