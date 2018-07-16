554ff8e96cc8e24e08954e5d7da785fa304ebb14
drillprop <drylbartosz@gmail.com>
2018-07-17 00:46:38 +0200

    added simple styling

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -3,12 +3,18 @@ import React, { Component } from "react";
 class TaskForm extends Component {
   render() {
     return (
-      <form>
+      <form id="task-form">
         <label htmlFor="task-title"> Task title </label>
         <input id="task-title" type="text" placeholder="Task" />
         <br />
         <label htmlFor="task-description">Task description</label>
-        <textarea id="task-description" type="text" placeholder="description" />
+        <textarea
+          id="task-description"
+          type="text"
+          placeholder="Description"
+          rows="5"
+          cols="20"
+        />
         <br />
         <label htmlFor="est-time">Estimated Time</label>
         <input id="est-time" type="number" placeholder="Estimated Time" />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import Router from "./Components/Router";
+import "./style.css";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
 registerServiceWorker();
