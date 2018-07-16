5ed25b5791f9a23b9c24d936089cf722482cdf3f
drillprop <drylbartosz@gmail.com>
2018-07-16 23:16:38 +0200

    added Home component and created route for it

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import NotFound from "./NotFound";
+import Home from "./Home";
 
 class Router extends Component {
   render() {
@@ -9,6 +10,7 @@ class Router extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={App} />
+          <Route path="/home" component={Home} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
