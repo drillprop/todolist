cc340bbfccd0fe3d16d4209dbed99a1f838a8f55
drillprop <drylbartosz@gmail.com>
2018-07-16 22:44:04 +0200

    Added Header component, added route to home in App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-
+import Header from "./Header";
+import { Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <Header />
+        <Link to="/home">
+          <button>Click Here to Continue</button>
+        </Link>
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>To Do List</h1>
+      <h3>Make your shit done.</h3>
+    </header>
+  );
+};
+
+export default Header;
