00bca0ee8467958b1f44d4230c35b1d9059b3d02
drillprop <drylbartosz@gmail.com>
2018-07-26 19:03:50 +0200

    added simple Navigation Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Navigation from "./Navigation";
 import { Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <Header />
         <Link to="/login">
           <button>Click Here to login</button>
---
+++ src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Logout</li>
+        <li>Register</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
