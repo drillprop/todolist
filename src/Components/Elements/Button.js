d6306cb56b17d40a3f1f410e5ac35ea1d76dc273
drillprop <drylbartosz@gmail.com>
2018-07-31 13:10:14 +0200

    made files for global styled Components like Forms

---
+++ src/Components/Elements/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  display: block;
+  margin: 30px auto;
+  border: none;
+  padding: 15px;
+  background: var(--accent-color);
+  text-transform: uppercase;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+`;
---
+++ src/Components/Elements/Forms.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin: 0 auto;
+  width: 300px;
+  height: 400px;
+  background: var(--main-color);
+`;
+
+export const Label = styled.label`
+  color: var(--main-bg-color);
+`;
