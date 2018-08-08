cffbfda70c3b38f7e112ebe7ce0ddf27c0a67b7a
drillprop <drylbartosz@gmail.com>
2018-08-09 01:23:14 +0200

    removed ugly yellow autofill background to forms, added as global style

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,11 +11,23 @@ injectGlobal`
   --accent-color:#CA3C25; 
 }
 
+
+
+
 * {
   font-family: 'Montserrat', sans-serif;
   box-sizing: content-box;
   color: var(--main-color)
 }
+
+{/* This block is fixing ugly yellowish background in chrome */}
+input:-webkit-autofill {
+-webkit-text-fill-color: var(--main-bg-color); 
+background-color:var(--main-color);
+shadow: 0 0 0px 10px black inset;
+transition: background-color 5000s ease-in-out 0s;
+}
+
 a {
   text-decoration: none;
   color:inherit;
