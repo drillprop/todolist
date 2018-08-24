094ebb122395a3876127dc806ad19b5ece9d6b17
drillprop <drylbartosz@gmail.com>
2018-08-24 05:53:59 +0200

    created basic menu

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 
 const Hamburger = styled.span`
-  color: var(--main-color);
-  height: 25px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: ${props =>
+    props.visible ? "var(--main-bg-color)" : "var(--main-color)"};
+  font-size: 50px;
+  cursor: pointer;
+  z-index: 10002;
+`;
+
+const Menu = styled.section`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+const List = styled.ul`
+  color: ${props =>
+    props.visible ? "var(--main-bg-color)" : "var(--main-color)"};
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 500px;
+  background: white;
+  z-index: 10001;
 `;
 
 class HamburgerMenu extends Component {
-  state = {};
+  state = {
+    menuVisible: false
+  };
+  makeVisible = () => {
+    this.setState({
+      menuVisible: !this.state.menuVisible
+    });
+  };
+
   render() {
-    return <Hamburger> &#9776</Hamburger>;
+    return (
+      <Menu>
+        <Hamburger visible={this.state.menuVisible} onClick={this.makeVisible}>
+          &#9776;
+        </Hamburger>
+        {this.state.menuVisible && (
+          <List visible={this.state.menuVisible}>
+            <li>Sign In</li>
+            <li>Sign Up</li>
+          </List>
+        )}
+      </Menu>
+    );
   }
 }
 
