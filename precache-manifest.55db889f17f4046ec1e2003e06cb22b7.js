1a00c1eecdbd751e1c1e2331e65870b54bdb05ec
abraxaswisz <abraxaswisz@gmail.com>
2019-08-29 18:11:20 +0200

    Updates

---
+++ precache-manifest.55db889f17f4046ec1e2003e06cb22b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db725d7baf5319a910aed4cb83196001",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "75a914f89df20cce2ff4",
+    "url": "/todolist/static/css/main.01b001fd.chunk.css"
+  },
+  {
+    "revision": "4c0af03fa7ad82deee1f",
+    "url": "/todolist/static/js/2.12f13344.chunk.js"
+  },
+  {
+    "revision": "75a914f89df20cce2ff4",
+    "url": "/todolist/static/js/main.90f1644f.chunk.js"
+  },
+  {
+    "revision": "1357b0ded239988e1ad0",
+    "url": "/todolist/static/js/runtime~main.15064748.js"
+  }
+]);
